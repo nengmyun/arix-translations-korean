42d6a3a64bcae04ea5817fc98d1a4d22b2ded6b5
macarong <44965898+nengmyun@users.noreply.github.com>
2024-02-05 22:50:13 +0900

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
-    'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'account-activity-log' => '계정 활동 로그',
+    'activity-log' => '활동 로그',
+    'clear-filters' => '필터 지우기',
+    'metadata' => '메타데이터',
+    'close' => '닫기',
+    'no-logs-server' => '이 서버에는 사용 가능한 활동 로그가 없습니다.'
 ];
