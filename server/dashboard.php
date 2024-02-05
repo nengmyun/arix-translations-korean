46178d0adf4b235a23c3bb2788ed2649a7b16301
macarong <44965898+nengmyun@users.noreply.github.com>
2024-02-05 22:53:02 +0900

    Update dashboard.php

--- server/dashboard.php
+++ server/dashboard.php
@@ -1,22 +1,22 @@
 <?php
 
 return [
-    'dashboard' => 'Dashboard',
+    'dashboard' => '대시보드',
 
     'sftp' => [
-        'title' => 'SFTP Details',
-        'server-address' => 'Server Address',
-        'username' => 'Username',
-        'description' => 'Your SFTP password is the same as the password you use to access this panel.',
-        'launch-sftp' => 'Launch SFTP',
+        'title' => 'SFTP 세부 정보',
+        'server-address' => '서버 주소',
+        'username' => '사용자 이름',
+        'description' => 'SFTP 암호는 이 패널에 액세스하는 데 사용하는 암호와 동일합니다.',
+        'launch-sftp' => 'SFTP 시작',
     ],
     'server-info' => [
-        'title' => 'Server Info',
-        'hostname' => 'Hostname',
-        'node-id' => 'Node ID',
-        'server-id' => 'Server ID',
+        'title' => '서버 정보',
+        'hostname' => '호스트명',
+        'node-id' => '노드 ID',
+        'server-id' => '서버 ID',
         
-        'uptime' => 'Uptime',
-        'server-ip' => 'Server IP',
+        'uptime' => '가동 시간',
+        'server-ip' => '서버 IP',
     ],
 ];
