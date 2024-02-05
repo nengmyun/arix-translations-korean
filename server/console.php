01f8aaa37f2b654fba7c7e16c7adc6331c14cd28
macarong <44965898+nengmyun@users.noreply.github.com>
2024-02-05 22:52:48 +0900

    Update console.php

--- server/console.php
+++ server/console.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'console' => 'Console',
-    'node-under-maintenance' => 'The node of this server is currently under maintenance and all actions are unavailable.',
-    'running-installation-process' => 'This server is currently running its installation process and most actions are unavailable.',
-    'being-transferred' => 'This server is currently being transferred to another node and all actions are unavailable.',
-    'type-a-command' => 'Type a command...',
+    'console' => '콘솔',
+    'node-under-maintenance' => '이 서버의 노드는 현재 유지 보수 중이며 모든 작업이 사용할 수 없습니다.',
+    'running-installation-process' => '이 서버는 현재 설치 프로세스를 실행 중이며 대부분의 작업이 사용할 수 없습니다.',
+    'being-transferred' => '이 서버는 현재 다른 노드로 전송 중이며 모든 작업이 사용할 수 없습니다.',
+    'type-a-command' => '명령을 입력하세요...',
 ];
