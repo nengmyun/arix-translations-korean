f2c3141ec0f42a027638c54531f813fb315f8b7f
macarong <44965898+nengmyun@users.noreply.github.com>
2024-02-05 22:54:29 +0900

    Update startup.php

--- server/startup.php
+++ server/startup.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'startup-settings' => 'Startup Settings',
-    'startup-command' => 'Startup Command',
-    'docker-image' => 'Docker Image',
-    'read-only' => 'Read Only',
-    'select-docker-feature' => 'This is an advanced feature allowing you to select a Docker image to use when running this server instance.',
-    'custom-docker-image' => 'This {"server\'s"} Docker image has been manually set by an administrator and cannot be changed through this UI.',
-    'variables' => 'Variables'
+    'startup-settings' => '시작 설정',
+    'startup-command' => '시작 명령',
+    'docker-image' => '도커 이미지',
+    'read-only' => '읽기 전용',
+    'select-docker-feature' => '이것은 이 서버 인스턴스를 실행할 때 사용할 도커 이미지를 선택하는 고급 기능입니다.',
+    'custom-docker-image' => '이 서버의 도커 이미지는 관리자에 의해 수동으로 설정되었으며 이 UI를 통해 변경할 수 없습니다.',
+    'variables' => '변수',
 ];
